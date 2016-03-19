ac5eca9ad9336fdbb344347b2e94afd35470d20f
Joe Love <git@joelove.uk>
2016-03-19 15:07:12 +0000

    update migration and seed data

--- migrations/20160319145300_addSamplesTable.js
+++ migrations/20160319145300_addSamplesTable.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('samples', function(table) {
       table.uuid('id').notNullable().primary();
+      table.string('page').notNullable();
       table.integer('count').defaultTo(0);
     }),
   ]);
--- test/seeds/seed.js
+++ test/seeds/seed.js
@@ -1,7 +1,8 @@
 export default (knex, Promise) => {
   return Promise.all([
     knex('samples').insert({
-      id: 'de305d54-75b4-431b-adb2-eb6b9e546014'
+      id: 'de305d54-75b4-431b-adb2-eb6b9e546014',
+      page: 'default'
     })
   ]);
 };
