aaf32f2b9fa3cdc3ce144e6ef658b2433eb8b0ae
Joe Love <git@joelove.uk>
2016-04-11 09:59:37 +0100

    refactor migration to fix ES6 style

--- migrations/20160319145300_addSamplesTable.js
+++ migrations/20160319145300_addSamplesTable.js
@@ -1,13 +1,11 @@
-exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('samples', function(table) {
-      table.uuid('id').notNullable().primary();
-      table.string('page').notNullable();
-      table.integer('count').defaultTo(0);
-    }),
-  ]);
-};
+exports.up = (knex, Promise) => Promise.all([
+  knex.schema.createTable('samples', table => {
+    table.uuid('id').notNullable().primary();
+    table.string('page').notNullable();
+    table.integer('count').defaultTo(0);
+  }),
+]);
 
-exports.down = function(knex, Promise) {
-  knex.schema.dropTable('samples');
-};
+exports.down = (knex, Promise) => Promise.all([
+  knex.schema.dropTable('samples'),
+]);
